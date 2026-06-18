bd623a6d5fe91fc77e837244a7d85590ade31d18
sxzyhub <geekycae@gmail.com>
2026-06-18 03:43:39 +0200

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-
-function showTab(id){document.querySelectorAll('.tab').forEach(t=>t.classList.remove('active'));document.getElementById(id).classList.add('active');}
-function toggleMenu(){document.getElementById('menu').classList.toggle('open');}
